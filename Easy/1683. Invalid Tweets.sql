f50abec45822989d3c247758a586514a23dd3e5a
Chang Ge <93849914+CarlyKeh@users.noreply.github.com>
2022-06-20 13:07:44 -0400

    Create 1683. Invalid Tweets.sql

---
+++ Easy/1683. Invalid Tweets.sql
@@ -0,0 +1,37 @@
+Write an SQL query to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+----------------------------------+
+| tweet_id | content                          |
++----------+----------------------------------+
+| 1        | Vote for Biden                   |
+| 2        | Let us make America great again! |
++----------+----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 14. It is a valid tweet.
+Tweet 2 has length = 32. It is an invalid tweet.
+
+-- Solution:
+SELECT tweet_id
+FROM Tweets
+WHERE CHAR_LENGTH(content)>15
+
+-- README:
+1. CHAR_LENGTH(content) # is equal to the string lenth
+2. CHAR_LENGTH('@') # is equal to 1
+3. LENGTH('@') is equal to 3
